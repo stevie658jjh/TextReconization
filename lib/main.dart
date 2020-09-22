377985b8b92657f8181f21f030f55feb1cecad63
Thien <nguyenthanhthien.658jjh@gmail.com>
2020-09-22 16:33:09 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/image_detail.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    // Retrieve the device cameras
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print(e);
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ML Vision',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CameraScreen(),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController _cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    _cameraController.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+
+    @override
+    void dispose() {
+      _cameraController.dispose();
+      super.dispose();
+    }
+  }
+
+  Future<String> _takePicture() async {
+    // Checking whether the controller is initialized
+    if (!_cameraController.value.isInitialized) {
+      print("Controller is not initialized");
+      return null;
+    }
+
+    // Formatting Date and Time
+    String dateTime = DateFormat.yMMMd()
+        .addPattern('-')
+        .add_Hms()
+        .format(DateTime.now())
+        .toString();
+
+    String formattedDateTime = dateTime.replaceAll(' ', '');
+    print("Formatted: $formattedDateTime");
+
+    // Retrieving the path for saving an image
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String visionDir = '${appDocDir.path}/Photos/Vision\ Images';
+    await Directory(visionDir).create(recursive: true);
+    final String imagePath = '$visionDir/image_$formattedDateTime.jpg';
+
+    // Checking whether the picture is being taken
+    // to prevent execution of the function again
+    // if previous execution has not ended
+    if (_cameraController.value.isTakingPicture) {
+      print("Processing is in progress...");
+      return null;
+    }
+
+    try {
+      // Captures the image and saves it to the
+      // provided path
+      await _cameraController.takePicture(imagePath);
+    } on CameraException catch (e) {
+      print("Camera Exception: $e");
+      return null;
+    }
+
+    return imagePath;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ML Vision'),
+      ),
+      body: _cameraController.value.isInitialized
+          ? Stack(
+        children: <Widget>[
+          CameraPreview(_cameraController),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton.icon(
+                icon: Icon(Icons.camera),
+                label: Text("Click"),
+                onPressed: () async {
+                  await _takePicture().then((String path) {
+                    if (path != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(path),
+                        ),
+                      );
+                    }
+                  });
+                },
+              ),
+            ),
+          )
+        ],
+      )
+          : Container(
+        color: Colors.black,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
